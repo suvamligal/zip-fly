4d1396371b0df0f667681f64870267ed2a788baa
Suvam Ligal <suvamligal@gmail.com>
2021-08-27 16:53:31 -0500

    springboot zip-fly first commit

---
+++ src/test/java/com/zipfly/zipfly/ZipFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipfly.zipfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
